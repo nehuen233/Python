32ec568e289dca888d521a80624d17e13ca60d08
danteuri <nehuenhugobarrios@gmail.com>
2024-05-08 15:22:48 -0300

    Update Ejercicio Python.py

--- Ejercicio Python.py
+++ Ejercicio Python.py
@@ -152,4 +152,8 @@ def ejercicio9():
     print("Año:", anio)
 
 #ejercicio10
+#En un curso de ciencias de la computación la calificación final del estudiante se determina a partir del
+#rendimiento en tres aspectos del trabajo. A) Existe una calificación por los exámenes parciales que representa el
+#30% del valor total de la nota final. B) la calificación por los trabajos prácticos corresponde al 20% del valor de la
+#nota final. C) el examen integrador que corresponde al 50% restante. (los valores de las notas pueden ir de 0 a 10)
 
