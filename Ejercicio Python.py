ad068430b7b434142bca344dd8cb45396de3b0ed
lautaroif08 <nehuenhugobarrios@gmail.com>
2024-05-13 13:59:04 -0300

    Update Ejercicio Python.py

--- Ejercicio Python.py
+++ Ejercicio Python.py
@@ -175,10 +175,17 @@ def ejercicio10():
 #pesos por cada auto vendido y un adicional del 5% del valor del auto vendido. Diseñar un algoritmo para calcular
 #el salario total del vendedor. Conociendo solamente el número de autos vendidos y el valor de venta de la unidad.
 def ejercicio11():
-    autos = int(input("Ingrese la cantidad vendida"))
-    valor = float(input("Ingrese el porcentaje del valor del coche"))
-    sueldo = (autos + valor * 5500)
+    autos = []
+    repeticiones = int(input("Ingrese la cantidad de autos que quiere ingresar"))
+    
+    for i in range(repeticiones):
+         valor = float(input("Ingrese el porcentaje del valor del coche"))
+         autos.append(valor)    
+    
+   
+    comision = repeticiones * 200
+    adicional = sum(autos) * 0.05
+    sueldototal = 5500 + comision + adicional 
 
-    print("El sueldo total es de:  ", sueldo)
+    print("El sueldo total es de:  ", sueldototal )
 
-ejercicio11()
